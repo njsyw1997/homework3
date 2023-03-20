9bbb876808334b1ed61eed549438178e4a9b216b
Georg Stadler <stadler@cims.nyu.edu>
2023-03-20 11:05:38 -0400

    adding serial omp scan

---
+++ omp-scan.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <stdio.h>
+#include <math.h>
+#include <omp.h>
+
+// Scan A array and write result into prefix_sum array;
+// use long data type to avoid overflow
+void scan_seq(long* prefix_sum, const long* A, long n) {
+  if (n == 0) return;
+  prefix_sum[0] = 0;
+  for (long i = 1; i < n; i++) {
+    prefix_sum[i] = prefix_sum[i-1] + A[i-1];
+  }
+}
+
+void scan_omp(long* prefix_sum, const long* A, long n) {
+  int p = omp_get_num_threads();
+  int t = omp_get_thread_num();
+  long size = n/p + 1;
+  long offset[p];
+  offset[0] = 0;
+#pragma parallel omp
+#pragma omp for schedule(static, size)
+  for (long i = 1; i < n; i++) {
+    prefix_sum[i] = prefix_sum[i-1] + A[i-1];
+  }
+  if (t == 0) {
+    for (j = 1; j < p; j++) {
+      offset[i] = offset[i-1] + A[i*size-1];
+    }
+  }
+#pragma omp for schedule(static, size)
+  for (long i = 1; i < n; i++) {
+    prefix_sum[i] = offset[t];
+  }
+}
+
+int main() {
+  long N = 100000000;
+  long* A = (long*) malloc(N * sizeof(long));
+  long* B0 = (long*) malloc(N * sizeof(long));
+  long* B1 = (long*) malloc(N * sizeof(long));
+  for (long i = 0; i < N; i++) A[i] = rand();
+  for (long i = 0; i < N; i++) B1[i] = 0;
+  
+  double tt = omp_get_wtime();
+  scan_seq(B0, A, N);
+  printf("sequential-scan = %fs\n", omp_get_wtime() - tt);
+
+  tt = omp_get_wtime();
+  scan_omp(B1, A, N);
+  printf("parallel-scan   = %fs\n", omp_get_wtime() - tt);
+
+  long err = 0;
+  for (long i = 0; i < N; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
+  printf("error = %ld\n", err);
+
+  free(A);
+  free(B0);
+  free(B1);
+  return 0;
+}
